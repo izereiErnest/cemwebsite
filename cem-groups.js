ea3b19a994b887c725b2ee7c027fcb9c4512f4ee
izereiErnest <izereiibrahym@gmail.com>
2025-01-07 06:57:44 +0200

    12

--- cem-groups.js
+++ cem-groups.js
@@ -3,10 +3,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const cards = document.querySelectorAll(".group-card");
 
-  cards.forEach(card => {
-    card.addEventListener("click", () => {
-      alert(`${card.innerText} content will load here.`);
-      // Replace the alert with logic to load the corresponding page
-    });
-  });
 });
